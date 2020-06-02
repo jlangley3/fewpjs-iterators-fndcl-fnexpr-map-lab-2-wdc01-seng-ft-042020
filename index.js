4878ffd75733da63cc0fdcf7b20bbdb07464c9bd
jlangley3 <jlangleyiii@aol.com>
2020-06-02 09:55:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const questions = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'how can you implement a Blockchain Web API?',
-  'what is Test Driven Development Workflow?',
-  'What is NaN and how can we check for it?',
-  'What is the difference between stopPropagation and preventDefault?',
-  'should you use immutable state and pure functions?',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+    'what does the this keyword mean?',
+    'What is the Contutorialuctor OO pattern?',
+    'how can you implement a Blockchain Web API?',
+    'what is Test Driven Development Workflow?',
+    'What is NaN and how can we check for it?',
+    'What is the difference between stopPropagation and preventDefault?',
+    'should you use immutable state and pure functions?',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
 ];
 
 const capitalizeSentence = () => {
-  return questions
+    return questions.map(question =>
+        question[0].toUpperCase() + question.slice(1)
+    );
 }
